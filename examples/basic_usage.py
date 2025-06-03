73d65fea78e27ebdf1d1dd173ad0bbdad401ff8b
Adrián Pino <adrian.pino@i2cat.net>
2025-06-03 12:04:26 +0200

    Rename sdk invocation from universal catalog to sdk catalog

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,8 +1,10 @@
-from src.common.universal_client_catalog import UniversalClientCatalog
+from src.common.sdk_catalog_client import SdkCatalogClient
+
+# "src.common.universal_catalog_client" would be equivalent to "sunrise6g_opensdk"
 
 
 def main():
-    # Hardcoded client configuration; here we would expect to load the config
+    # The module importing the SDK, loads the config
     client_specs = {
         "edgecloud": {
             "client_name": "i2edge",
@@ -11,19 +13,24 @@ def main():
         # ,
         # "network": {
         #     "client_name": "open5gs",
-        #     "base_url": "http://IP:PORT"
+        #     "base_url": "http://IP:PORT",
+        #     "scs_as_id": "id_example"
         # }
     }
 
-    clients = UniversalClientCatalog.create_clients(client_specs)
+    clients = SdkCatalogClient.create_clients(client_specs)
 
     edgecloud_client = clients.get("edgecloud")
     # network_client = clients.get("network")
 
-    # Example usage
-    print("Running test endpoint: get_edge_cloud_zones:")
+    # Example of edgecloud client in action
+    print("Testing edgecloud client function: get_edge_cloud_zones:")
     zones = edgecloud_client.get_edge_cloud_zones()
     print(zones)
+
+    # # Example of network client in action
+    # print("Testing network client function: EXAMPLE_FUNCTION:")
+    # network_client.get_qod_session(session_id="example_session_id")
 
 
 if __name__ == "__main__":
