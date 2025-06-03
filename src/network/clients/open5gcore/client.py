6f8cccf5f5a782d03a1a779d146e86d39187e4a0
Adrián Pino <adrian.pino@i2cat.net>
2025-06-03 18:40:02 +0200

    Update placeholder for open5gcore so the invocation test succed

--- src/network/clients/open5gcore/client.py
+++ src/network/clients/open5gcore/client.py
@@ -1,30 +1,14 @@
 # -*- coding: utf-8 -*-
-from typing import Dict
-
 from src import logger
 from src.network.core.network_interface import NetworkManagementInterface
 
 log = logger.get_logger(__name__)
 
 
-# Placeholder for the Open5gcore Network Management Client
+# TODO: Define any specific parameters or methods needed for Open5GCore
+# In case any functionality is not implemented, raise NotImplementedError
+
+
 class NetworkManager(NetworkManagementInterface):
     def __init__(self, base_url: str, scs_as_id: str):
         pass
-
-    def create_qod_session(self, session_info: Dict) -> Dict:
-        pass
-
-    def get_qod_session(self, session_id: str) -> Dict:
-        pass
-
-    def delete_qod_session(self, session_id: str) -> None:
-        pass
-
-
-# Note:
-# As this class is inheriting from NetworkManagementInterface, it is
-# expected to implement all the abstract methods defined in that interface.
-#
-# In case this network adapter doesn't support a specific method, it should
-# be marked as NotImplementedError.
