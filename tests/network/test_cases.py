0c1233bd3518cd2bb033ced3407780a88c569c98
giuliocarot0 <giulio.carota@outlook.it>
2025-06-16 14:57:20 +0200

    add oai to test cases

--- tests/network/test_cases.py
+++ tests/network/test_cases.py
@@ -2,9 +2,16 @@
 test_cases = [
     {
         "network": {
+            "client_name": "oai",
+            "base_url": "http://localhost/",
+            "scs_as_id": "scs",
+        }
+    },
+    {
+        "network": {
             "client_name": "open5gs",
             "base_url": "http://192.168.124.233:8082/",
             "scs_as_id": "scs",
         }
-    }
+    },
 ]
