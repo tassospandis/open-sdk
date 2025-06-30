3b569ede0d29d4ecb276d4b8e0667e64232e106a
Adrián Pino <adrian.pino@i2cat.net>
2025-06-30 12:54:09 +0200

    Anonymize test config variables

--- tests/edgecloud/test_cases.py
+++ tests/edgecloud/test_cases.py
@@ -3,30 +3,30 @@ test_cases = [
     # {
     #     "edgecloud": {
     #         "client_name": "i2edge",
-    #         "base_url": "http://192.168.123.48:30769/",
-    #         "flavour_id": "67f3a0b0e3184a85952e174d",
+    #         "base_url": "http://X.Y.Z.T:PORT/",
+    #         "flavour_id": "<>",
     #     }
     # },
     # {
     #     "edgecloud": {
     #         "client_name": "aeros",
-    #         "base_url": "http://test-aeros.url",
-    #         "aerOS_API_URL": "http://fake.api.url",
-    #         "aerOS_ACCESS_TOKEN": "fake-access",
-    #         "aerOS_HLO_TOKEN": "fake-hlo"
+    #         "base_url": "http://X.Y.Z.T:PORT/",
+    #         "aerOS_API_URL": "http://A.B.C.D:PORT",
+    #         "aerOS_ACCESS_TOKEN": "<>",
+    #         "aerOS_HLO_TOKEN": "<>"
     #     }
     # },
     # {
     {
         "edgecloud": {
             "client_name": "kubernetes",
-            "base_url": "http://146.124.106.200/k8s",
+            "base_url": "http://X.Y.Z.T:PORT/",
             # Additional parameters for K8s client:
-            "PLATFORM_PROVIDER": "ICOM",
-            "KUBERNETES_MASTER_TOKEN": "T3FRNnNVK25FY3I5ZHlNYmxrSEFpd2VPcW5WTlliTnRVNVo3bitNY1B3az0K",
+            "PLATFORM_PROVIDER": "<>",
+            "KUBERNETES_MASTER_TOKEN": "<>",
             # "KUBERNETES_MASTER_PORT": "80",
-            "KUBERNETES_USERNAME": "user",
-            "EMP_STORAGE_URI": "mongodb://146.124.106.200:32411",
+            "KUBERNETES_USERNAME": "<>",
+            "EMP_STORAGE_URI": "mongodb://A.B.C.D:PORT",
             "K8S_NAMESPACE": "sunrise6g",
         }
     },
